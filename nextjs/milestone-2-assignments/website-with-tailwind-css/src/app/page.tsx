8a96b1fb543be59f11ded1401967b76d2376485b
MadihaAli1 <madihmali57@gmail.com>
2024-11-27 06:51:22 -0800

    add assignnments

--- nextjs/milestone-2-assignments/website-with-tailwind-css/src/app/page.tsx
+++ nextjs/milestone-2-assignments/website-with-tailwind-css/src/app/page.tsx
@@ -1,13 +1,13 @@
-import './styles/button.css'
+import './styles/button.css';
 
 function Homepage() {
   return (
-    <div className="flex items-center justify-center min-100" >
+    <div className="flex items-center justify-center min-100">
       <div className="content w-full text-center text-white">
         <h1 className="text-[62px] mt-[80px]">Enhance Your Beauty</h1>
         <p className="mt-5 mb-5 mx-auto font-thin leading-[25px]">
-          "Enjoy exceptional beauty and grooming services focused on quality and <br />
-          your satisfaction. Book your appointment today and treat yourself!"
+          &quot;Enjoy exceptional beauty and grooming services focused on quality and <br />
+          your satisfaction. Book your appointment today and treat yourself!&quot;
         </p>
         <div>
           <button type="button" className="btn relative overflow-hidden">
